619d5411fd60e00080fb86986d810979f3de9edc
bluntpencil254 <makorimike9@gmail.com>
2025-10-26 19:54:18 +0300

    quiz game for understanding types of collections

---
+++ quizgame.py
@@ -0,0 +1,49 @@
+questions = ("When did Kenya gain independence? ", 
+             "Who was the first president of Kenya? ",
+             "How many counties does Kenya have? ",
+             "What is the capital city of Kenya? ",
+             "Who is the governor of the capital city? ")
+
+choices =  (("A. 1925", "B. 1960", "C. 1963", "D. 1966"), 
+            ("A. Tom Mboya", "B. Jomo Kenyatta", "C. Oginga Odinga", "D. Nelson Mandela"), 
+            ("A. 12", "B. 40", "C. 36", "D. 47"), 
+            ("A. Mombasa", "B. Kisumu", "C. Kitale", "D. Nairobi"), 
+            ("A. Jonnson Sakaja", "B. Mike Sonko", "C. Idah Odinga", "D. Willis Raburu"))
+
+answers = ("C", "B", "D", "D", "A")
+
+question_num = 0
+score = 0
+guesses = []
+
+
+print("-----Quiz Time-----")
+print()
+
+for question in questions:
+    print("----------------------")
+    print(question)
+
+    for choice in choices[question_num]:
+        print(choice)
+    print()
+
+    guess = input("Enter: A, B, C, or D: ").upper()
+
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        score += 1
+        print("Correct")
+    elif guess != answers:
+        print(f"{guess} is not a valid answer. The correct answer is {answers[question_num]}")
+    elif guess == "":
+        print(f"You did not choose. The correct answer is {answers[question_num]}")
+    else:
+        print(f"Incorrect. The correct answer is {answers[question_num]}")
+
+    question_num += 1
+
+print("Thanks for playing, Byee")
+
+
+    
